5bd7073f4edb45758bdb811e931e0337980dca6c
Will Da Silva <will@willdasilva.xyz>
2018-01-01 13:18:40 -0500

    Add solutions for day 14

---
+++ 14-1.py
@@ -0,0 +1,15 @@
+with open('14.in', 'r') as puzzleInputFile:
+    reindeer = (x.strip() for x in puzzleInputFile.readlines())
+
+# (rest, fly, speed, [state, ticks, distance])
+stats = lambda x: (int(x[13]), int(x[6]), int(x[3]), [1, int(x[6]), int(x[3])])
+reindeer = [stats(x.split()) for x in reindeer]
+for _ in range(2503):
+    for x in reindeer:
+        x[3][1] -= 1 # tick
+        if not x[3][1]: # if ticks == 0: change state
+            x[3][0] = not x[3][0]
+            x[3][1] = x[x[3][0]]
+        if x[3][0]: # if flying: move
+            x[3][2] += x[2]
+print(max(reindeer, key=lambda x: x[3][2])[3][2])
