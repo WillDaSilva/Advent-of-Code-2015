598c699c9a7a3c79145f66e7a31b0055552b49d6
Will Da Silva <will@willdasilva.xyz>
2017-12-31 23:32:40 -0500

    Add solutions for day 12

---
+++ 12-1.py
@@ -0,0 +1,17 @@
+import json
+
+with open('12.in', 'r') as puzzleInputFile:
+    puzzleInput = json.load(puzzleInputFile)
+
+def parse(struct):
+    total = 0
+    for x in struct:
+        if type(x) == int:
+            total += x
+        elif type(x) == dict:
+            total += parse(x.values())
+        elif type(x) == list:
+            total += parse(x)
+    return total
+
+print(parse(puzzleInput.values()))
